e9b25de9abcbb418dd2789fc70cc022df86956ff
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 12:21:29 -0300

    refactor: add global plugin compatibility wrapper
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ LSTelegramNotify.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src/Plugin/LSTelegramNotifyPlugin.php';
+
+class LSTelegramNotify extends \LibreCodeCoop\LSTelegramNotify\Plugin\LSTelegramNotifyPlugin
+{
+}
