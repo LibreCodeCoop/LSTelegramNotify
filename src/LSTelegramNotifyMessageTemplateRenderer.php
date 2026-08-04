d2fb74b2111de0c5e633340bf7ba850ca390cbf2
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 12:30:57 -0300

    refactor: drop redundant message renderer wrapper
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/LSTelegramNotifyMessageTemplateRenderer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Template/MessageTemplateRenderer.php';
-
-class LSTelegramNotifyMessageTemplateRenderer extends \LibreCodeCoop\LSTelegramNotify\Template\MessageTemplateRenderer
-{
-}
