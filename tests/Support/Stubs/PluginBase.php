d5c49439a237b360de54f6021e3cc61c506cb361
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 19:09:00 -0300

    Adjust PluginBase test stub
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Support/Stubs/PluginBase.php
+++ tests/Support/Stubs/PluginBase.php
@@ -1,5 +1,36 @@
 <?php
 
+class PluginEvent
+{
+	public function get(string $name)
+	{
+		return null;
+	}
+
+	public function set(string $name, $value): void
+	{
+	}
+}
+
 class PluginBase
 {
+	public string $id = 'LSTelegramNotify';
+
+	protected function subscribe(string $eventName): void
+	{
+	}
+
+	protected function getEvent()
+	{
+		return new PluginEvent();
+	}
+
+	protected function get($key = null, $model = null, $id = null, $default = null)
+	{
+		return $default;
+	}
+
+	protected function set($name, $value, $model = null, $id = null): void
+	{
+	}
 }
