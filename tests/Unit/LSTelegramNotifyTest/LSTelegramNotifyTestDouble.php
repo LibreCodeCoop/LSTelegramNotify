3f60bea4cd8104d361c8da1776603fc11e668fcb
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 12:21:16 -0300

    test: allow plugin double to mock template fields
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Unit/LSTelegramNotifyTest/LSTelegramNotifyTestDouble.php
+++ tests/Unit/LSTelegramNotifyTest/LSTelegramNotifyTestDouble.php
@@ -5,10 +5,19 @@ class LSTelegramNotifyTestDouble extends \LSTelegramNotify
     /** @var array<string, mixed> */
     private array $mockedSettings = [];
 
+    /** @var array<string, array<string, string>> */
+    private array $mockedTemplateFields = [];
+
     /** @param array<string, mixed> $settings */
     public function setMockedSettings(array $settings): void
     {
         $this->mockedSettings = $settings;
+    }
+
+    /** @param array<string, array<string, string>> $fields */
+    public function setMockedTemplateFields(array $fields): void
+    {
+        $this->mockedTemplateFields = $fields;
     }
 
     protected function get($key = null, $model = null, $id = null, $default = null)
@@ -16,5 +25,11 @@ class LSTelegramNotifyTestDouble extends \LSTelegramNotify
         return array_key_exists((string) $key, $this->mockedSettings)
             ? $this->mockedSettings[(string) $key]
             : $default;
+    }
+
+    /** @return array<string, array<string, string>> */
+    protected function getTemplateFieldValues(int $surveyId, int $responseId): array
+    {
+        return $this->mockedTemplateFields;
     }
 }
