b583890cd2edbdac83fcf0e9d1cd7eee9374b690
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 12:30:57 -0300

    refactor: drop redundant field value provider wrapper
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/LSTelegramNotifySurveyFieldValueProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'Survey/SurveyFieldValueProvider.php';
-
-class LSTelegramNotifySurveyFieldValueProvider extends \LibreCodeCoop\LSTelegramNotify\Survey\SurveyFieldValueProvider
-{
-}
