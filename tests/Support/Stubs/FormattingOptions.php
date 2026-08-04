53367c674698607909c432fb5427c27878535272
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-04 19:09:00 -0300

    Adjust FormattingOptions test stub
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Support/Stubs/FormattingOptions.php
+++ tests/Support/Stubs/FormattingOptions.php
@@ -2,4 +2,15 @@
 
 class FormattingOptions
 {
+	public int $responseMinRecord = 0;
+	public int $responseMaxRecord = 0;
+
+	/** @var array<int, string|int> */
+	public array $selectedColumns = [];
+
+	public string $responseCompletionState = '';
+	public string $headingFormat = '';
+	public string $answerFormat = '';
+	public string $csvFieldSeparator = ',';
+	public string $output = '';
 }
