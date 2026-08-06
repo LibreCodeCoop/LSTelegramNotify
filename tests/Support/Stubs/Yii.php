95628f5650b84035622f5ed6f9b7cbf59207400d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-08-05 21:16:37 -0300

    test: stub yii client script registration
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/Support/Stubs/Yii.php
+++ tests/Support/Stubs/Yii.php
@@ -1,7 +1,14 @@
 <?php
 
+class LSYii_ClientScript
+{
+    public const POS_POSTSCRIPT = 5;
+}
+
 class Yii
 {
+    public static array $registeredScripts = [];
+
     public static function import($name, $force = false)
     {
     }
@@ -12,6 +19,28 @@ class Yii
             public function getLanguage()
             {
                 return 'en';
+            }
+
+            public function getPluginManager()
+            {
+                return \AppRuntimeMock::$pluginManager ?? new class {
+                    public function dispatchEvent($event, $plugin = null): void
+                    {
+                    }
+                };
+            }
+
+            public function getClientScript()
+            {
+                return new class {
+                    public function registerScript(string $id, string $script, int $position = 0): void
+                    {
+                        Yii::$registeredScripts[$id] = [
+                            'script' => $script,
+                            'position' => $position,
+                        ];
+                    }
+                };
             }
         };
     }
