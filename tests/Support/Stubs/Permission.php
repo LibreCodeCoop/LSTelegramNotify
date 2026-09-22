6bca2974852b93704b3d0bd8ceaa74f322c263b5
Vitor Mattos <vitor@php.rio>
2026-09-22 20:44:43 -0300

    Support global permission checks in tests
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- tests/Support/Stubs/Permission.php
+++ tests/Support/Stubs/Permission.php
@@ -5,6 +5,9 @@ class Permission
     /** @var callable|null */
     public static $hasSurveyPermissionHandler;
 
+    /** @var callable|null */
+    public static $hasGlobalPermissionHandler;
+
     public static function model()
     {
         return new class {
@@ -12,6 +15,15 @@ class Permission
             {
                 if (is_callable(Permission::$hasSurveyPermissionHandler)) {
                     return call_user_func(Permission::$hasSurveyPermissionHandler, $surveyId, $permission, $crud);
+                }
+
+                return true;
+            }
+
+            public function hasGlobalPermission($permission, $crud = null)
+            {
+                if (is_callable(Permission::$hasGlobalPermissionHandler)) {
+                    return call_user_func(Permission::$hasGlobalPermissionHandler, $permission, $crud);
                 }
 
                 return true;
