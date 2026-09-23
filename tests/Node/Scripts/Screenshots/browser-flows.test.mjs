3dbd4e0a6dfe88fd4d1c5836df16f378f585da71
Vitor Mattos <vitor@php.rio>
2026-09-23 12:39:38 -0300

    test: import setting container helper

--- tests/Node/Scripts/Screenshots/browser-flows.test.mjs
+++ tests/Node/Scripts/Screenshots/browser-flows.test.mjs
@@ -5,6 +5,7 @@ import {
   buildPluginActionSelector,
   enableCustomMessageSettings,
   fillTelegramConnectionSettings,
+  getPluginSettingContainer,
   extractPluginInstallRequest,
   getPluginManagerPageUrl,
   getPluginManagerScanFilesUrl,
